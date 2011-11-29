f2e8439d18496f84c79b7d3441faaa5874257f0d
Léo Unbekandt <leo@unbekandt.eu>
2011-11-29 16:09:51 +0100

    Gestion des utilisateurs avec devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,7 @@ Bds::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3001' }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Bds::Application.routes.draw do
+  devise_for :users
+
   root :to => 'home#index'
+	
+	namespace :admin do
+		root :to => '#index'
+		resources :articles
+	end
+
 end
